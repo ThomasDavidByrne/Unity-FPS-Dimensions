cca103b1baf15ed351bea2ae05eff036479c703c
Thomas Byrne <thomasbyrne15@hotmail.com>
2018-04-05 15:33:40 +0100

    Commit with Gems
    Added 4 objects that have different affects on the player

---
+++ FPS_v1/ProjectSettings/Assets/Scripts/Coin.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Coin : MonoBehaviour {
+
+	public int RotateSpeed;
+	public AudioSource CollectSound;
+	public GameObject TheGem;
+	public GameObject pickupEffect;
+
+	void Update () {
+		transform.Rotate (0, RotateSpeed, 0, Space.World );
+
+	}
+	void OnTriggerEnter( Collider other ){
+		if (other.CompareTag("Player"))
+		{
+			Pickup(other);
+		}
+	}
+	void Pickup(Collider player)
+	{
+		CollectSound.Play ();
+		TheGem.SetActive (false);
+		GameObject effect = Instantiate (pickupEffect, transform.position, transform.rotation);
+		Destroy (effect, 1.8f);
+		ScorePlus (player);
+	}
+	void ScorePlus (Collider Player)
+	{
+		playerStats stats = Player.GetComponent<playerStats> ();
+		stats.score++;
+	}
+}
---
+++ FPS_v1/ProjectSettings/Assets/Scripts/GemRotate.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GemRotate : MonoBehaviour {
+
+	public int RotateSpeed;
+	public AudioSource CollectSound;
+	public GameObject TheGem;
+	public GameObject pickupEffect;
+	public float multiplier;
+
+	void Update () {
+		transform.Rotate (0, RotateSpeed, 0, Space.World );
+		
+	}
+	void OnTriggerEnter( Collider other ){
+		if (other.CompareTag("Player"))
+		{
+			Pickup(other);
+		}
+	}
+	void Pickup(Collider player)
+	{
+		CollectSound.Play ();
+		TheGem.SetActive (false);
+		GameObject effect = Instantiate (pickupEffect, transform.position, transform.rotation);
+		Destroy (effect, 1.8f);
+		bigUp(player);
+		healthUp (player);
+	}
+	void bigUp(Collider player)
+	{
+		player.transform.localScale *= multiplier;
+	}
+	void healthUp(Collider Player)
+	{
+		playerStats stats = Player.GetComponent<playerStats> ();
+		stats.health *=  multiplier;
+	}
+
+	
+}
+
+

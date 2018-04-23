80ad0d32908077e29af76e0c538fe4e668965817
Thomas Byrne <thomasbyrne15@hotmail.com>
2018-04-23 15:46:20 +0100

    Test Level updated
    Gives helaht value and lives value and score value that works with the gems aswell as adding a ui bar that has the health

---
+++ FPS_v1/UnityPackageManager/Assets/Scripts/PearGem.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PearGem : MonoBehaviour {
+
+	public int RotateSpeed;
+	public AudioSource CollectSound;
+	public GameObject TheGem;
+	public GameObject pickupEffect;
+	public float multiplier;
+
+	void Update () {
+		transform.Rotate (0, RotateSpeed, 0, Space.World );
+
+	}
+	void OnTriggerEnter( Collider other ){
+		if (other.CompareTag("Player"))
+		{
+			Pickup(other);
+		}
+	}
+	void Pickup(Collider player)
+	{
+		CollectSound.Play ();
+		TheGem.SetActive (false);
+		GameObject effect = Instantiate (pickupEffect, transform.position, transform.rotation);
+		Destroy (effect, 1.8f);
+		healthUp (player);
+	}
+	void healthUp(Collider Player)
+	{
+		playerStats stats = Player.GetComponent<playerStats> ();
+		stats.health *=  multiplier;
+	}
+}
